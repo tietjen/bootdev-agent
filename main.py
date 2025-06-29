0e03ff3514759e56064081c875198058bd4de6c3
tietjen <jan.tietjen@parasec.de>
2025-06-29 18:07:50 +0200

    use types.Content to keep the whole conversation

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -16,8 +18,11 @@ client = genai.Client(api_key=api_key)
 
 if len(sys.argv) > 1:
     prompt = sys.argv[1]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=prompt
+        model='gemini-2.0-flash-001', contents=messages,
         )
     print(response.text)
 else:
